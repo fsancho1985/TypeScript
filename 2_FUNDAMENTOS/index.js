0b24afcc845136517628fa33c247dab3e2ef30e0
fsancho <frsancho1985@gmail.com>
2023-04-24 17:51:19 -0300

    Fundamentos

--- 2_FUNDAMENTOS/index.js
+++ 2_FUNDAMENTOS/index.js
@@ -1,9 +1,31 @@
+"use strict";
 // 1 - Numbers
-var x = 10;
+let x = 10;
 console.log(x);
 x = 16;
 console.log(typeof x);
-var y = 15.6546546;
+const y = 15.6546546;
 console.log(y);
 console.log(typeof y);
 console.log(y.toPrecision(4));
+// 2 - Strings
+const firstName = "Francisco";
+console.log(firstName.toUpperCase());
+let fullName;
+const lastName = "Sancho";
+fullName = firstName + " " + lastName;
+console.log(typeof fullName);
+// 3 - Booleanos
+let a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
+console.log(typeof a);
+// 4 - Inference and annotation
+let ann = "Teste";
+let inf = "Teste2";
+// ann = 1
+// inf = 1
+console.log(inf.charAt(2));
+console.log('Testing');
--- 2_FUNDAMENTOS/index.ts
+++ 2_FUNDAMENTOS/index.ts
@@ -12,3 +12,35 @@ const y: number = 15.6546546
 console.log(y)
 console.log(typeof y)
 console.log(y.toPrecision(4))
+
+// 2 - Strings
+const firstName: string = "Francisco"
+
+console.log(firstName.toUpperCase())
+
+let fullName: string
+
+const lastName: string = "Sancho"
+
+fullName = firstName + " " + lastName
+
+console.log(typeof fullName)
+
+// 3 - Booleanos
+let a: boolean = false
+console.log(a)
+console.log(typeof a)
+a = true
+console.log(a)
+console.log(typeof a)
+
+// 4 - Inference and annotation
+let ann: string = "Teste"
+let inf = "Teste2"
+
+// ann = 1
+// inf = 1
+
+console.log(inf.charAt(2))
+
+console.log('Testing')
